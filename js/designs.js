ea809e8a0f62fba8a5128c8264ec41a4e9d856f5
Igor Todorovic <igor333m@yahoo.com>
2017-09-27 21:50:24 -0700

    Defining variables and storing data

--- js/designs.js
+++ js/designs.js
@@ -1,6 +1,18 @@
-// Select color input
-// Select size input
+let color, gridHeight, gridWidth;
 
+// Select color input
+$('#color_picker').change(function() {
+	color = $(this).val();
+});
+
+// Select size input
+$('#input_height').change(function() {
+	gridHeight = $(this).val();
+});
+
+$('#input_width').change(function() {
+	gridWidth = $(this).val();
+});
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
@@ -8,3 +20,4 @@ function makeGrid() {
 // Your code goes here!
 
 }
+console.log(color);
